1a5bd8af6586443df8a3bbd5dc10d843ae238a43
rafatul <rafatulislam@inneed.cloud>
2024-01-16 18:37:17 +0600

    Git action23

--- cypress/pages/BaseUrl.js
+++ cypress/pages/BaseUrl.js
@@ -1,12 +1,12 @@
 class  BaseUrl {
     visitpage () {
-         //cy.visit("https://trmis.inneed.cloud");
-         cy.visit("http://localhost:5173");
+         cy.visit("https://trmis.inneed.cloud");
+         //cy.visit("http://localhost:5173");
          cy.wait(1000);
      }
      visitadmindashboard () {
-       // cy.visit('https://trmis.inneed.cloud/')
-        cy.visit('http://localhost:5173/')
+       cy.visit('https://trmis.inneed.cloud/')
+        //cy.visit('http://localhost:5173/')
         .window()
         .then((win) => {
             win.history.pushState({}, '', 'login');
